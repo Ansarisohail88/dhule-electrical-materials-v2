f4f2ca92957a0e15c5c3145f44b024e0990eae20
Ansarisohail88 <ansarisohail12527@gmail.com>
2026-08-05 14:24:03 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/dhule-electrical-materials-v2/',
+  plugins: [react()],
+});
